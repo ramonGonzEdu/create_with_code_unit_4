5f55591aa46e7fd24c96c703dacd221194052e14
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-01-22 09:51:09 -0500

    better movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
 	public float speed = 5.0f;
 	private Rigidbody playerRb;
-
+	private GameObject focalPoint;
 
 	private void Start()
 	{
 		playerRb = GetComponent<Rigidbody>();
-
+		focalPoint = GameObject.Find("Focal Point");
 	}
 
 	private void Update()
 	{
 		float forwardInput = Input.GetAxis("Vertical");
-		playerRb.AddForce(Vector3.forward * speed * forwardInput);
+		playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
 	}
 }
