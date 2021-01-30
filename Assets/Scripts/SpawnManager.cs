b2e2024bf1fa1cf7ce38f4f0f770ee9f88df9dd6
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-01-29 19:45:37 -0500

    random enemies

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+	private void Start()
+	{
+		var copy = Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+		var EnemyScript = copy.GetComponent<Enemy>();
+		EnemyScript.speed = Random.Range(5, 15);
+	}
+
+	private void Update()
+	{
+
+	}
+}
