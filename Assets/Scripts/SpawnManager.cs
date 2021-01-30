bd8b76e06eca402dd4586b7573c02fb608b93389
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-01-29 19:49:23 -0500

    refactor

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,15 @@ public class SpawnManager : MonoBehaviour
 	private float spawnRange = 9;
 	private void Start()
 	{
-		Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+		Vector3 spawnPos = GenerateSpawnPosition();
 		var copy = Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
 		var EnemyScript = copy.GetComponent<Enemy>();
 		EnemyScript.speed = Random.Range(5, 15);
+	}
+
+	private Vector3 GenerateSpawnPosition()
+	{
+		return new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
 	}
 
 	private void Update()
