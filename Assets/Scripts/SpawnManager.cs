c01fd5ba76d4a293922f54bf11d4e914a96b8c88
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-01-29 19:48:34 -0500

    random enemy positions

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,9 +3,11 @@
 public class SpawnManager : MonoBehaviour
 {
 	public GameObject enemyPrefab;
+	private float spawnRange = 9;
 	private void Start()
 	{
-		var copy = Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+		Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+		var copy = Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
 		var EnemyScript = copy.GetComponent<Enemy>();
 		EnemyScript.speed = Random.Range(5, 15);
 	}
